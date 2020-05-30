8f76b8eabff2cf2acccb4c002d343fad78327bc1
iceyglaceon <theiceyglaceon@gmail.com>
2020-05-30 16:43:41 -0700

    first commit

---
+++ ChannelPoints_to_ChannelCurrency_StreamlabsSystem.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+
+# Importing Libraries Needed
+import clr, codecs, json, os, re, sys, threading, datetime
+
+clr.AddReference("IronPython.Modules.dll")
+clr.AddReferenceToFileAndPath(os.path.join(os.path.dirname(os.path.realpath(__file__)) + "\References", "TwitchLib.PubSub.dll"))
+from TwitchLib.PubSub import TwitchPubSub
+
+
+#   Required Script Information
+ScriptName = "Twitch Channel Points to Currency"
+Website = "https://www.twitch.tv/IceyGlaceon"
+Description = "Script to trigger automagically add currency for users that use channel point reward redemptions."
+Creator = "IceyGlaceon"
+Version = "1.0.0.2"
+
+#   Define Global Variables
+SettingsFile = os.path.join(os.path.dirname(__file__), "settings.json")
+README = os.path.join(os.path.dirname(__file__), "README.md")
+EventReceiver = None
+ThreadQueue = []
+CurrentThread = None
+PlayNextAt = datetime.datetime.now()
+
+#---------------------------------------
+# Classes
+#---------------------------------------
+class Settings(object):
+    def __init__(self, SettingsFile=None):
+        if SettingsFile and os.path.isfile(SettingsFile):
+            with codecs.open(SettingsFile, encoding="utf-8-sig", mode="r") as f:
+                self.__dict__ = json.load(f, encoding="utf-8")
+        else:
+            self.EnableDebug = True
+            self.TwitchOAuthToken = ""
+            self.TwitchReward1Name = ""
+            self.R1Points = 1000
+            self.TwitchReward2Name = ""
+            self.R2Points = 1000
+            self.TwitchReward3Name = ""
+            self.R3Points = 1000
+            self.TwitchReward4Name = ""
+            self.R4Points = 1000
+
+
+    def Reload(self, jsondata):
+        self.__dict__ = json.loads(jsondata, encoding="utf-8")
+        return
+
+    def Save(self, SettingsFile):
+        try:
+            with codecs.open(SettingsFile, encoding="utf-8-sig", mode="w+") as f:
+                json.dump(self.__dict__, f, encoding="utf-8")
+            with codecs.open(SettingsFile.replace("json", "js"), encoding="utf-8-sig", mode="w+") as f:
+                f.write("var settings = {0};".format(json.dumps(self.__dict__, encoding='utf-8')))
+        except:
+            Parent.Log(ScriptName, "Failed to save settings to the file. Fix error and try again.")
+        return
+
+#   [Required] Initialize Data (Only called on load)
+def Init():
+    global ScriptSettings
+    ScriptSettings = Settings(SettingsFile)
+    ScriptSettings.Save(SettingsFile)
+
+    ## Init the Streamlabs Event Receiver
+    Start()
+
+    return
+
+def Start():
+    if ScriptSettings.EnableDebug:
+        Parent.Log(ScriptName, "Starting receiver");
+
+    global EventReceiver
+    EventReceiver = TwitchPubSub()
+    EventReceiver.OnPubSubServiceConnected += EventReceiverConnected
+    EventReceiver.OnRewardRedeemed += EventReceiverRewardRedeemed
+
+    EventReceiver.Connect()
+
+def EventReceiverConnected(sender, e):
+
+    if ScriptSettings.EnableDebug:
+        Parent.Log(ScriptName, "Event receiver connecting")
+# -----------------------------------
+#  Get Channel id for Username
+# -----------------------------------
+    headers = {
+        "Client-ID": "icyqwwpy744ugu5x4ymyt6jqrnpxso",
+        "Authorization": "Bearer " + ScriptSettings.TwitchOAuthToken[6:]
+    }
+    result = json.loads(Parent.GetRequest("https://api.twitch.tv/helix/users?login=" + Parent.GetChannelName(), headers))
+    if ScriptSettings.EnableDebug:
+        Parent.Log(ScriptName, "result: " + str(result))
+    user = json.loads(result["response"])
+    id = user["data"][0]["id"]
+
+    if ScriptSettings.EnableDebug:
+        Parent.Log(ScriptName, "Event receiver connected, sending topics for channel id: " + id)
+
+    EventReceiver.ListenToRewards(id)
+    EventReceiver.SendTopics(ScriptSettings.TwitchOAuthToken)
+    return
+
+# -----------------------------------
+#  Actual Conversion and Application Done Here
+# -----------------------------------
+def EventReceiverRewardRedeemed(sender, e):
+    if ScriptSettings.EnableDebug:
+        Parent.Log(ScriptName, "Event triggered")
+        # e.DisplayName == streamlabs chatbot data.UserName. e.Login == streamlabs chatbot data.User
+        # This does NOT transfer equally for youtube/mixer. Use resources in README for alternatives.
+        Parent.Log(ScriptName, "Testing twitch reward name: " + e.RewardTitle)
+        Parent.Log(ScriptName, "This is the TwitchReward1Name: " + ScriptSettings.TwitchReward1Name)
+        dataUser = e.Login
+        dataUserName = e.DisplayName
+        reward = e.RewardTitle
+    if e.RewardTitle == ScriptSettings.TwitchReward1Name:
+        ThreadQueue.append(threading.Thread(target=RewardRedeemedWorker,args=(reward, ScriptSettings.R1Points, dataUser, dataUserName)))
+    if e.RewardTitle == ScriptSettings.TwitchReward2Name:
+        ThreadQueue.append(threading.Thread(target=RewardRedeemedWorker,args=(reward, ScriptSettings.R1Points, dataUser, dataUserName)))
+    if e.RewardTitle == ScriptSettings.TwitchReward3Name:
+        ThreadQueue.append(threading.Thread(target=RewardRedeemedWorker,args=(reward, ScriptSettings.R1Points, dataUser, dataUserName)))
+    if e.RewardTitle == ScriptSettings.TwitchReward4Name:
+        ThreadQueue.append(threading.Thread(target=RewardRedeemedWorker,args=(reward, ScriptSettings.R1Points, dataUser, dataUserName)))
+    return
+
+def RewardRedeemedWorker(reward, amount, dataUser, dataUserName):
+    if ScriptSettings.EnableDebug:
+        Parent.Log(ScriptName, dataUserName + " is redeeming " + reward + " for " + str(amount) + " units worth of channel currency")
+
+    Parent.AddPoints(dataUser,dataUserName,long(str(amount)))
+
+    global PlayNextAt
+    PlayNextAt = datetime.datetime.now() + datetime.timedelta(0, delay)
+
+
+#---------------------------
+#   [Required] Execute Data / Process messages
+#---------------------------
+def Execute(data):
+    return
+
+#---------------------------
+#   [Required] Tick method (Gets called during every iteration even when there is no incoming data)
+#---------------------------
+def Tick():
+
+    global PlayNextAt
+    if PlayNextAt > datetime.datetime.now():
+        return
+
+    global CurrentThread
+    if CurrentThread and CurrentThread.isAlive() == False:
+        CurrentThread = None
+
+    if CurrentThread == None and len(ThreadQueue) > 0:
+        if ScriptSettings.EnableDebug:
+            Parent.Log(ScriptName, "Starting new thread. " + str(PlayNextAt))
+        CurrentThread = ThreadQueue.pop(0)
+        CurrentThread.start()
+
+    return
+
+#---------------------------
+#   [Optional] Parse method (Allows you to create your own custom $parameters)
+#---------------------------
+def Parse(parseString, userid, username, targetid, targetname, message):
+    return parseString
+#---------------------------
+#   [Optional] Reload Settings (Called when a user clicks the Save Settings button in the Chatbot UI)
+#---------------------------
+def ReloadSettings(jsonData):
+    # Execute json reloading here
+
+    if ScriptSettings.EnableDebug:
+        Parent.Log(ScriptName, "Saving settings.")
+
+    global EventReceiver
+    try:
+        if EventReceiver:
+            EventReceiver.Disconnect()
+
+        ScriptSettings.__dict__ = json.loads(jsonData)
+        ScriptSettings.Save(SettingsFile)
+        EventReceiver = None
+
+        Start()
+        if ScriptSettings.EnableDebug:
+            Parent.Log(ScriptName, "Settings saved successfully")
+    except Exception as e:
+        if ScriptSettings.EnableDebug:
+            Parent.Log(ScriptName, str(e))
+
+    return
+
+#---------------------------
+#   [Optional] Unload (Called when a user reloads their scripts or closes the bot / cleanup stuff)
+#---------------------------
+def Unload():
+    # Disconnect EventReceiver cleanly
+    try:
+        if EventReceiver:
+            EventReceiver.Disconnect()
+    except:
+        if ScriptSettings.EnableDebug:
+            Parent.Log(ScriptName, "Event receiver already disconnected")
+
+    return
+
+#---------------------------
+#   [Optional] ScriptToggled (Notifies you when a user disables your script or enables it)
+#---------------------------
+def ScriptToggled(state):
+    return
+
+def openreadme():
+    os.startfile(README)
+
+def GetToken():
+	os.startfile("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=icyqwwpy744ugu5x4ymyt6jqrnpxso&redirect_uri=https://twitchapps.com/tmi/&scope=channel:read:redemptions&force_verify=true")
---
+++ README.md
@@ -0,0 +1,42 @@
+# SLCB Channel Points to Channel Currency Script
+
+This script allows users to use exchange their earned channel points for your personal channel currency!
+This bot does not have a cool down and should work even if someone redeems in rapid succession.
+
+## Installing
+
+This script was built for use with Streamlabs Chatbot.
+Follow instructions on how to install custom script packs at:
+https://github.com/StreamlabsSupport/Streamlabs-Chatbot/wiki/Prepare-&-Import-Scripts
+
+Click [Here](https://github.com/Encrypted-Thoughts/SLCB-ChannelPointsSFXTrigger/blob/master/ChannelPointsSFXTrigger.zip?raw=true) to download the script pack directly.
+
+Once installed you will need to provide an oAuth token. You can get one by clicking the Get Token button in script settings.
+This button also exists in the streamlabs chatbot UI. Make sure you don't show this token on stream, it is as sensitive
+as your stream key!
+
+![token](https://user-images.githubusercontent.com/50642352/82402817-f8165480-9a22-11ea-8810-fc93899d785a.png)
+
+## Use
+
+Once installed, just add custom channel point rewards to your twitch channel and then _exactly_ match the names of the reward to a Twitch Reward event in the script settings within Streamlabs OBS. In this example, please refer to `Point Testing` that costs `25 Channel Points`
+
+![image](https://user-images.githubusercontent.com/50642352/82403376-4ed05e00-9a24-11ea-80c9-17751ff32085.png)
+
+![image2](https://user-images.githubusercontent.com/50642352/82403434-71fb0d80-9a24-11ea-860d-5c7d8cafe4e7.png)
+
+## Author
+
+IceyGlaceon - [Twitch](https://www.twitch.tv/iceyglaceon), [Twitter](https://www.twitter.com/theiceyglaceon)
+
+
+## References
+
+This script makes use of TwitchLib's pubsub listener to detect the channel point redemptions. Go check out their repo at https://github.com/TwitchLib/TwitchLib for more info.
+
+This script is based off of [EncryptedThoughts's Channel Point's SFX Trigger.](https://github.com/Encrypted-Thoughts/SLCB-ChannelPointsSFXTrigger)
+Check out their [Twitch](https://www.twitch.tv/EncryptedThoughts)!
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
